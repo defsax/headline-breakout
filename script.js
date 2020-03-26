19afea7d9c4b509e41b9964b2dcd47059a7b5c86
Per <defayetteperry@gmail.com>
2020-03-26 09:44:34 -0400

    Lesson 1: Drawing, canvas, context.

---
+++ script.js
@@ -0,0 +1,25 @@
+window.onload = () => {
+  //reference canvas
+  var canvas = document.getElementById("myCanvas");
+  //store rendering context
+  var ctx = canvas.getContext("2d");
+  
+  //print a red square
+  ctx.beginPath();
+  ctx.rect(20, 40, 50, 50);
+  ctx.fillStyle = "#FF0000";
+  ctx.fill();
+  ctx.closePath();
+  
+  ctx.beginPath();
+  ctx.arc(240, 160, 100, 0, Math.PI*2, false);
+  ctx.fillStyle = "green";
+  ctx.fill();
+  ctx.closePath();
+  
+  ctx.beginPath();
+  ctx.rect(160, 10, 100, 40);
+  ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+  ctx.stroke();
+  ctx.closePath();
+}
