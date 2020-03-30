2359a10f642f4fc4576b885bc903ea7a98f987ea
Per <defayetteperry@gmail.com>
2020-03-30 16:51:26 -0400

    Modified paddle physics, so ball angle will change depending on where it hits on the paddle. Lots still to do.

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ window.onload = () => {
   var rightPressed = false;
   var leftPressed = false;
   
+  //timing vars
+  var deltaTime = 0.0;
+  var lastFrame = 0.0;
+  var intervalSpeed = 10;
+  
   document.addEventListener("keydown", keyDownHandler, false);
   document.addEventListener("keyup", keyUpHandler, false);
   
@@ -22,9 +27,9 @@ window.onload = () => {
     x : 50,
     y : canvas.height-30,
     r : 10,
-    v1 : 10, v2 : 10,
-    dx : 2,
-    dy : -2,
+    dx : 0.1,
+    dy : -0.1,
+    speed : 0.3,
     col : getRandomColor(),
     getObject : function(){
       return "ball";
@@ -37,8 +42,8 @@ window.onload = () => {
     w : 50,
     h : 50,
     v1 : 0, v2 : 50,
-    dx : -2,
-    dy : 2,
+    dx : -0.1,
+    dy : 0.1,
     getObject : function(){
       return "square";
     }
@@ -52,21 +57,30 @@ window.onload = () => {
   };
 
   function refresh(){
+    //var currFrame = new Date();
+    //deltaTime = currFrame - lastFrame;
+    //lastFrame = currFrame;
+    
     //refresh context
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
-    drawText("HELlo WorLD", 10, 50, "white", "stroke");
-     
-    checkBoundaries(ball);
+    drawText(ball.x.toString(), 10, 25, "black", "fill");
+    drawText(ball.y.toString(), 10, 50, "black", "fill");
+    
+    checkBoundaries(ball, intervalSpeed);
+    ball.col = getRandomColor();
     drawCirc(ball, ball.col, "fill");
+    //checkBoundaries(square, deltaTime);    
+    //drawRect(square, "red", "fill");
     
-    checkBoundaries(square);    
-    drawRect(square, "red", "fill");
-    
-    paddleInput();
+    paddleInput(intervalSpeed);
     drawPaddle(paddle);
+
+    //document.getElementById("title").innerHTML = "deltaTime: " + deltaTime;
+    //console.log(currTime.getTime());
+    
   }
-  var interval = setInterval(refresh, 50);
+  var interval = setInterval(refresh, intervalSpeed);
   
   function drawCirc(obj, color, type){
     ctx.beginPath();
@@ -125,108 +139,80 @@ window.onload = () => {
     ctx.beginPath();
     ctx.rect(obj.x, obj.y, obj.w, obj.h);
     ctx.fillStyle = "black";
+    ctx.strokeStyle = "white";
     ctx.fill();
+    ctx.stroke();
     ctx.closePath();
   }
   
-  function paddleInput(){
+  function paddleInput(dt){
     if(rightPressed){
-      paddle.x += 7;
+      paddle.x += 0.7 * dt;
       if(paddle.x + paddle.w > canvas.width)
         paddle.x = canvas.width - paddle.w;
     }
     else if(leftPressed){
-      paddle.x -= 7;
+      paddle.x -= 0.7 * dt;
       if(paddle.x < 0)
         paddle.x = 0;
     }
   }
   
-  function checkBoundaries(obj){
+  function checkBoundaries(obj, dt){
     //check top/bottom boundaries
-    if(obj.y + obj.dy < obj.v1){
-      obj.dy = calcNewDir(obj.dy);
+    if(obj.y + obj.dy < obj.r){
+      obj.dy = -obj.dy;
+      
       if(obj.getObject() === "ball")
         obj.col = getRandomColor();
     } 
-    else if(obj.y + obj.dy > canvas.height - obj.v2){
+    else if(obj.y + obj.dy > canvas.height - obj.r){
       alert("GAME OVER");
       document.location.reload();
       clearInterval(interval); //needed for chrome
     }
     //check left/right boundaries
-    if(obj.x + obj.dx < obj.v1 || obj.x + obj.dx > canvas.width - obj.v2){
-      obj.dx = calcNewDir(obj.dx);
+    if(obj.x + obj.dx < obj.r || obj.x + obj.dx > canvas.width - obj.r){
+      obj.dx = -obj.dx;
       if(obj.getObject() === "ball")
         obj.col = getRandomColor();
     }
-    /*
-    //check if object hit paddle
-    if(obj.y + obj.dy + obj.v2 > paddle.y && obj.x + obj.dx < paddle.x + paddle.w && obj.x + obj.dx > paddle.x) //paddle right collision
-      obj.dx = calcNewDir(obj.dx);
-    if(obj.y + obj.dy + obj.v2 > paddle.y && obj.x + obj.dx + obj.v2 > paddle.x && obj.x + obj.dx + obj.v2 < paddle.x + paddle.w) //paddle left collision
-      obj.dx = calcNewDir(obj.dx);
-    if ( (obj.x > paddle.x && obj.x < paddle.x + paddle.w) || ((obj.x + obj.w) > paddle.x && (obj.x < paddle.x + paddle.w) &&
-      obj.y + obj.dy + obj.v2 > paddle.y))
-    {
-      obj.dy = calcNewDir(obj.dy);
-      console.log("paddle collision");
+
+    if(AABBcollision(obj, paddle)){
+      calculateNewAngle(obj);
+      obj.col = getRandomColor();
     }
-    */
-    checkPaddleCollision(obj);
-    
     
     //update position
-    obj.x += obj.dx;
-    obj.y += obj.dy;
+    obj.x += obj.dx * dt * obj.speed;
+    obj.y += obj.dy * dt * obj.speed;
   }
   
-  function checkPaddleCollision(obj){
-    if(obj.x < paddle.x + paddle.w &&
-        obj.x + obj.v2 > paddle.x    &&
-        obj.y < paddle.y + paddle.h  &&
-        obj.y + obj.v2 > paddle.y){
-        
-        if(obj.x < paddle.x + paddle.w && 
-          obj.x + obj.v2 > paddle.x + paddle.w &&
-          obj.y + obj.v2 > paddle.y + 3){
-          if(rightPressed)
-            paddle.x -= 10;
-          console.log("paddle right collision");
-          //obj.x = paddle.x + paddle.w + 1;
-          obj.dx = calcNewDir(obj.dx);
-          return;
-        }
-        
-        if(obj.x + obj.v2 > paddle.x && 
-          obj.x < paddle.x && 
-          obj.y + obj.v2 > paddle.y + 3){
-            if(leftPressed)
-              paddle.x += 10;
-            console.log("paddle left collision");
-            //obj.x = paddle.x - obj.v2 - 1;
-            obj.dx = calcNewDir(obj.dx);
-            return;
-          }
-        
-        if(obj.y < paddle.y + paddle.h)
-          console.log("paddle bottom collision");
-        
-        if(obj.y + obj.v2 > paddle.y){
-          console.log("paddle top collision");
-          obj.y = paddle.y - obj.v2 -1;
-          obj.dy = calcNewDir(obj.dy);
-          return;
-        }
-      }
+  function calculateNewAngle(obj){
+
+    var paddleCenter = paddle.x + (paddle.w / 2);
+    var centerDist = (paddle.x + (paddle.w / 2)) - (obj.x);
+  
+    var normalizeIntersect = centerDist / (paddle.w / 2);
+    var newBounceAngle = normalizeIntersect * (5*Math.PI/12);
+    
+    obj.dx = Math.sin(newBounceAngle);
+    obj.dy = -Math.cos(newBounceAngle);
+    obj.dx= -obj.dx;
   }
   
-  function calcNewDir(dir){
-    if(dir < 0){
-      return (Math.floor(Math.random() * 2) + 1);
-    } else {
-      return -(Math.floor(Math.random() * 2) + 1);
-    }
+  function AABBcollision(obj1, obj2){
+    if(obj1.x < obj2.x + obj2.w   &&
+        obj1.x + obj1.r > obj2.x &&
+        obj1.y < obj2.y + obj2.h  &&
+        obj1.y + obj1.r > obj2.y)
+      return true;
+    else
+      return false;
+  }
+  
+  function normalize(val, max, min){
+    return (val - min) / (max - min);
   }
   
   function getRandomColor(){
