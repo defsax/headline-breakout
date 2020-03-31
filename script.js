ba174fd54cf064185ea986b7dd83bff369632bee
Per <defayetteperry@gmail.com>
2020-03-31 13:27:49 -0400

    Ball now sticks to paddle until spacebar is pressed. Then ball is launched at random angle.

--- script.js
+++ script.js
@@ -9,10 +9,11 @@ window.onload = () => {
   var rightPressed = false;
   var leftPressed = false;
   
-  //timing vars
-  var deltaTime = 0.0;
-  var lastFrame = 0.0;
+  //timing var
   var intervalSpeed = 10;
+  
+  //game active bool
+  var ballAttached = true;
   
   document.addEventListener("keydown", keyDownHandler, false);
   document.addEventListener("keyup", keyUpHandler, false);
@@ -23,9 +24,17 @@ window.onload = () => {
   canvas.height = window.innerHeight;
   console.log("canvas.width: " + canvas.width);
   
+  var paddle = {
+    w : 75, 
+    h : 10,
+    x : (canvas.width-75) / 2,
+    y : canvas.height - (10 * 3),
+    speed : 0.3
+  };
+  
   var ball = {
-    x : 50,
-    y : canvas.height-30,
+    x : paddle.x + paddle.w / 2,
+    y : paddle.y - 10,
     r : 10,
     dx : 0.1,
     dy : -0.1,
@@ -35,43 +44,28 @@ window.onload = () => {
       return "ball";
     }
   };
-  
-  var square = {
-    x : 100,
-    y : 100,
-    w : 50,
-    h : 50,
-    v1 : 0, v2 : 50,
-    dx : -0.1,
-    dy : 0.1,
-    getObject : function(){
-      return "square";
-    }
-  };
-  
-  var paddle = {
-    w : 75, 
-    h : 10,
-    x : (canvas.width-75) / 2,
-    y : canvas.height - (10 * 3)
-  };
 
   function refresh(){
-    //var currFrame = new Date();
-    //deltaTime = currFrame - lastFrame;
-    //lastFrame = currFrame;
     
     //refresh context
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
-    drawText(ball.x.toString(), 10, 25, "black", "fill");
-    drawText(ball.y.toString(), 10, 50, "black", "fill");
+    //round ball vars to nearest hundredth
+    var xRounded = ball.x.toFixed(2);
+    var yRounded = ball.y.toFixed(2);
+    drawText(xRounded.toString(), 10, 25, "black", "fill");
+    drawText(yRounded.toString(), 10, 50, "black", "fill");
     
-    checkBoundaries(ball, intervalSpeed);
-    ball.col = getRandomColor();
-    drawCirc(ball, ball.col, "fill");
-    //checkBoundaries(square, deltaTime);    
-    //drawRect(square, "red", "fill");
+    if(ballAttached === true){
+      ball.x = paddle.x + (paddle.w / 2);
+      ball.y = paddle.y - ball.r;
+      drawCirc(ball, ball.col, "fill");
+    }
+    else{
+      checkBoundaries(ball, intervalSpeed);
+      ball.col = getRandomColor(); //random color every frame
+      drawCirc(ball, ball.col, "fill");
+    }
     
     paddleInput(intervalSpeed);
     drawPaddle(paddle);
@@ -147,12 +141,12 @@ window.onload = () => {
   
   function paddleInput(dt){
     if(rightPressed){
-      paddle.x += 0.7 * dt;
+      paddle.x += 1 * dt * paddle.speed;
       if(paddle.x + paddle.w > canvas.width)
         paddle.x = canvas.width - paddle.w;
     }
     else if(leftPressed){
-      paddle.x -= 0.7 * dt;
+      paddle.x -= 1 * dt * paddle.speed;
       if(paddle.x < 0)
         paddle.x = 0;
     }
@@ -178,6 +172,7 @@ window.onload = () => {
         obj.col = getRandomColor();
     }
 
+    //check ball collision with paddle
     if(AABBcollision(obj, paddle)){
       calculateNewAngle(obj);
       obj.col = getRandomColor();
@@ -191,14 +186,26 @@ window.onload = () => {
   function calculateNewAngle(obj){
 
     var paddleCenter = paddle.x + (paddle.w / 2);
-    var centerDist = (paddle.x + (paddle.w / 2)) - (obj.x);
+    var distanceFromCenter = (paddle.x + (paddle.w / 2)) - (obj.x);
   
-    var normalizeIntersect = centerDist / (paddle.w / 2);
+    var normalizeIntersect = distanceFromCenter / (paddle.w / 2);
     var newBounceAngle = normalizeIntersect * (5*Math.PI/12);
     
     obj.dx = Math.sin(newBounceAngle);
     obj.dy = -Math.cos(newBounceAngle);
     obj.dx= -obj.dx;
+  }
+  
+  function calculateStartAngle(obj){
+    var newBounceAngle = getRndFloat(-1, 1) * (5*Math.PI/12);
+    
+    obj.dx = Math.sin(newBounceAngle);
+    obj.dy = -Math.cos(newBounceAngle);
+    obj.dx= -obj.dx;
+  }
+  
+  function getRndFloat(min, max) {
+    return Math.random() * (max - min) + min;
   }
   
   function AABBcollision(obj1, obj2){
@@ -209,10 +216,6 @@ window.onload = () => {
       return true;
     else
       return false;
-  }
-  
-  function normalize(val, max, min){
-    return (val - min) / (max - min);
   }
   
   function getRandomColor(){
@@ -227,14 +230,20 @@ window.onload = () => {
   function keyDownHandler(e){
     if(e.key == "Right" || e.key == "ArrowRight")
       rightPressed = true;
-    
     else if(e.key == "Left" || e.key == "ArrowLeft")
       leftPressed = true;
+    else if(e.keyCode == 32 && ballAttached === true){
+      console.log("Space pressed.");
+      calculateStartAngle(ball);
+      ballAttached = false;
+    }
   }
   function keyUpHandler(e){
     if(e.key == "Right" || e.key == "ArrowRight")
       rightPressed = false;
     else if(e.key == "Left" || e.key == "ArrowLeft")
       leftPressed = false;
+    else if(e.keyCode == 32)
+      console.log("Space released.");
   }
 }
